baf31ff84a5fda6bcc9283795d26ef321f79e875
miguel angel jaramillo flores <miguel_runita@hotmail.com>
2021-10-20 02:30:57 -0600

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../theme";
+import createEmotionCache from "../createEmotionCache";
+import { AppProps } from "next/dist/shared/lib/router/router";
+
+const clientSideEmotionCache = createEmotionCache();
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: AppProps) {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Tyerra challenge</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default MyApp;
+
+// Client-side cache, shared for the whole session of the user in the browser.
